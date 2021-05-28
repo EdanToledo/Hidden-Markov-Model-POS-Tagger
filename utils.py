9acec41d501c60ced39215fe846fd16802c4bc04
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-05-28 14:36:34 +0200

    Add function to calculate emission probability table and bigram probability table

--- utils.py
+++ utils.py
@@ -41,6 +41,7 @@ def get_vocab_counts(training_sentences):
     prev_tag = "<s>"
 
     for sentence in training_sentences:
+        total_tag_counts["<s>"]+=1
         for (word,tag) in sentence:
             total_tag_counts[tag]+=1
             double_tag_counts[(prev_tag,tag)]+=1
@@ -62,6 +63,24 @@ def get_bigram_prob_laplace(prev_tag,tag,double_tag_counts,total_tag_counts):
 def get_emission_prob(word,tag,word_tag_counts,total_tag_counts):
     return word_tag_counts[(word,tag)]/total_tag_counts[tag]
 
+def get_emission_prob_table(word_tag_counts,total_tag_counts):
+    table = {}
+    for (word,_) in word_tag_counts.items():
+        table[word] = {}
+        for tag in total_tag_counts:
+            table[word][tag] = get_emission_prob(word,tag,word_tag_counts,total_tag_counts)
+
+    return table
+
+def get_bigram_prob_table(double_tag_counts,total_tag_counts,bigram_prob_func):
+    table = {}
+    for (prev_tag,_) in double_tag_counts:
+        table[prev_tag] = {}
+        for tag in total_tag_counts:
+            table[prev_tag][tag] = bigram_prob_func(prev_tag,tag,double,total_tag_counts)
+
+    return table
+
 def get_sentence_tag_prob(bi_gram_prob_func,sentence_tags,double_tag_counts,total_tag_counts):
     prob = 0
     prev_tag = "<s>"
@@ -78,12 +97,12 @@ def linear_interpolation_smoothing(bi_lamba,bi_prob,uni_lambda,uni_prob):
 
 # what about y0 == <s>
 def viterbi(sentence, double_tag_counts, word_tags_counts, total_tag_counts):
-    pi = [[0]*len(total_tag_counts)]*len(sentence)
+    pi = [[0]*len(total_tag_counts)]*(len(sentence)+1)
     prev_tag = "<s>"
     pi[0][0] = 1
     for i, (word, tag) in enumerate(sentence):
         for j,t in enumerate(total_tag_counts):
-            pi = get_emission_prob(word, t, word_tags_counts, total_tag_counts) * get_bigram_prob_laplace(prev_tag, t, double_tag_counts,total_tag_counts) * pi[i][j]
+            pi[i+1][j] = get_emission_prob(word, t, word_tags_counts, total_tag_counts) * get_bigram_prob_laplace(prev_tag, t, double_tag_counts,total_tag_counts) * pi[i][j]
         prev_tag = tag
 
     
