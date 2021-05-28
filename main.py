7af2c864073060084dba2bbcadd210703c92c613
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-05-29 00:14:43 +0200

    bug still missing- cant seem to find it.

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ def main():
     sentences = utils.split_sentences(df)
     total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(sentences)
     
-    df = utils.read_csv("TestSet.csv")
+    #df = utils.read_csv("TestSet.csv")
     sentences = utils.split_sentences(df)
     tot_count=0
     tot_ground=0
@@ -17,11 +17,13 @@ def main():
         sentence_words = [word for (word,_) in sentence]
         # print(sentence_words)
         result = utils.viterbi(sentence_words,double_tag_counts,word_tag_counts,total_tag_counts)
-        # print(result)
+        
         count,total = (utils.eval(result,sentence))
         tot_count += count
         tot_ground +=total
-    print("The models shows a",tot_count/tot_ground*100, " percentage accuracy!")
+        print("The models shows a",tot_count/tot_ground*100, " percentage accuracy!")
+        if i == 0:
+            break
         
         
 
--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-from numpy import double
+from numpy import double, set_printoptions
 from sklearn.model_selection import train_test_split
 import pandas as pd
 import os
@@ -10,17 +10,17 @@ def split_sentences(df):
     tokens = df['Token']
     sentences = []
     sen = []
-    sen.append(("<s>","<s>"))   
+    sen.append(("<s>","START"))   
     for i in range (0, len(pos)):
-        if (pd.isna(pos[i])):
-            sen.append(("</s>","</s>"))
+        if (pd.isna(tokens[i])):
+            sen.append(("</s>","END"))
             sentences.append(sen)
 
             sen = []   
-            sen.append(("<s>","<s>"))   
+            sen.append(("<s>","START"))   
         else:
             sen.append((tokens[i], pos[i]))
-    sen.append(("</s>","</s>"))
+    sen.append(("</s>","END"))
     sentences.append(sen)        
 
     return sentences
@@ -43,13 +43,14 @@ def get_vocab_counts(training_sentences):
     double_tag_counts = defaultdict(int)
     word_tag_counts = defaultdict(int)
     
-
     for sentence in training_sentences:
-        total_tag_counts["<s>"]+=1
-        prev_tag = "<s>"
+        total_tag_counts["START"]+=1
+        prev_tag = "START"
+        word_tag_counts[("<s>","START")]+=1
         for i,(word,tag) in enumerate(sentence):
             if i == 0:
                 continue
+            
             total_tag_counts[tag]+=1
             double_tag_counts[(prev_tag,tag)]+=1
             word_tag_counts[(word,tag)]+=1
@@ -62,7 +63,7 @@ def get_bigram_prob_laplace(prev_tag,tag,double_tag_counts,total_tag_counts):
     if ((prev_tag,tag) in double_tag_counts):
         return (double_tag_counts[(prev_tag,tag)]+1)/(total_tag_counts[prev_tag] + len(total_tag_counts))
     else:
-        return 0
+        return 1/(total_tag_counts[prev_tag] + len(total_tag_counts))
 
 
 def get_emission_prob(word,tag,word_tag_counts,total_tag_counts):
@@ -94,15 +95,16 @@ def linear_interpolation_smoothing(bi_lamba,bi_prob,uni_lambda,uni_prob):
     return bi_lamba*bi_prob+uni_lambda*uni_prob
 
 # what about y0 == <s>
-def viterbi(sentence, double_tag_counts, word_tags_counts, total_tag_counts):
+def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
     pi = defaultdict(lambda: defaultdict(float))
     bp = defaultdict(dict)
-    emission_table = get_emission_prob_table(word_tags_counts,total_tag_counts)
+    emission_table = get_emission_prob_table(word_tag_counts,total_tag_counts)
     bigram_table = get_bigram_prob_table(double_tag_counts,total_tag_counts,get_bigram_prob_laplace)
 
+    
     max_tag = ""
     prev_word = "<s>"
-    prev_tag = "<s>"
+    prev_tag = "START"
     pi[prev_word][prev_tag] = 1
     
     for word in (sentence):
@@ -122,28 +124,22 @@ def viterbi(sentence, double_tag_counts, word_tags_counts, total_tag_counts):
         prev_word = word
 
     result = []
-    tag = "</s>"
+    tag = "END"
     for i in range(len(sentence)-1,0,-1):
-        # print("WORD:",sentence[i])
-        # print("TAG:",tag)
-        # result.append((sentence[i],tag))
-        # tag = bp[sentence[i]][tag]
-        if (bp[sentence[i]][tag] != ''): #Uknown
-            result.append((sentence[i],tag))
-            tag = bp[sentence[i]][tag]
-        else:
-            result.append((sentence[i],"UNK")) #HOW TO HANLDE UNK IN TERMS OF NEXT TAG
-        # print(bp[sentence[i]][tag])
-    result.append(("<s>","<s>"))   #dp not registering last s tags, so changes loop to not include it.
+        result.append((sentence[i],tag))
+        tag = bp[sentence[i]][tag]
+    result.append(("<s>","START"))
 
     return result[::-1]
+
 
 
 def eval(result,ground_truth):
     count_true = 0
     for i,(word,pos) in enumerate(result):
         (true_word,true_pos) = ground_truth[i]
+        
         if word == true_word and pos == true_pos:
             count_true+=1
     
-    return count_true-2,len(ground_truth)
+    return count_true,len(ground_truth)
