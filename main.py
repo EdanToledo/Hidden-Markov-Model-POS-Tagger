0ba9c3602c4ca2ba8c48b192ddcece6ca3b6ed16
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-05-28 18:00:45 +0200

    finished initial viterbi algo

--- main.py
+++ main.py
@@ -9,11 +9,13 @@ def main():
     total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(sentences)
     
     sentence = sentences[0]
+    sentence_words = []
     for (word,tag) in sentence:
-        print(word,end=" ")
+        sentence_words.append(word)
 
-    print("\n\n\n")
-    utils.viterbi(sentence,double_tag_counts,word_tag_counts,total_tag_counts)
+
+    result = utils.viterbi(sentence_words,double_tag_counts,word_tag_counts,total_tag_counts)
+    print(result)
     
 if __name__ == "__main__":
     main()
--- utils.py
+++ utils.py
@@ -10,12 +10,17 @@ def split_sentences(df):
     tokens = df['Token']
     sentences = []
     sen = []
+    sen.append(("<s>","<s>"))   
     for i in range (0, len(pos)):
         if (pd.isna(pos[i])):
+            sen.append(("</s>","</s>"))
             sentences.append(sen)
-            sen = []      
+
+            sen = []   
+            sen.append(("<s>","<s>"))   
         else:
             sen.append((tokens[i], pos[i]))
+    sen.append(("</s>","</s>"))
     sentences.append(sen)        
 
     return sentences
@@ -37,19 +42,18 @@ def get_vocab_counts(training_sentences):
     total_tag_counts = defaultdict(int)
     double_tag_counts = defaultdict(int)
     word_tag_counts = defaultdict(int)
-    prev_tag = "<s>"
+    
 
     for sentence in training_sentences:
         total_tag_counts["<s>"]+=1
-        for (word,tag) in sentence:
+        prev_tag = "<s>"
+        for i,(word,tag) in enumerate(sentence):
+            if i == 0:
+                continue
             total_tag_counts[tag]+=1
             double_tag_counts[(prev_tag,tag)]+=1
             word_tag_counts[(word,tag)]+=1
             prev_tag = tag
-
-        double_tag_counts[(prev_tag,"</s>")] +=1
-        total_tag_counts["</s>"]+=1
-
 
     return total_tag_counts, double_tag_counts, word_tag_counts
 
@@ -86,17 +90,6 @@ def get_bigram_prob_table(double_tag_counts,total_tag_counts,bigram_prob_func):
        
     return table
 
-def get_sentence_tag_prob(bi_gram_prob_func,sentence_tags,double_tag_counts,total_tag_counts):
-    prob = 0
-    prev_tag = "<s>"
-    for tag in sentence_tags:
-        prob += math.log(bi_gram_prob_func(prev_tag,tag,double_tag_counts,total_tag_counts))
-        prev_tag = tag
-    prob += math.log(bi_gram_prob_func(prev_tag,"</s>",double_tag_counts,total_tag_counts))
-
-    return prob
-         
-
 def linear_interpolation_smoothing(bi_lamba,bi_prob,uni_lambda,uni_prob):
     return bi_lamba*bi_prob+uni_lambda*uni_prob
 
@@ -106,24 +99,33 @@ def viterbi(sentence, double_tag_counts, word_tags_counts, total_tag_counts):
     bp = defaultdict(dict)
     emission_table = get_emission_prob_table(word_tags_counts,total_tag_counts)
     bigram_table = get_bigram_prob_table(double_tag_counts,total_tag_counts,get_bigram_prob_laplace)
-    max_word = "<s>"
-    max_tag = "<s>"
+
+    max_tag = ""
     prev_word = "<s>"
     prev_tag = "<s>"
     pi[prev_word][prev_tag] = 1
-
+    
     for word in (sentence):
+        
         for tag in total_tag_counts:
+            
             for prev_tag in total_tag_counts:
+               
                 prob = (emission_table[word][tag] * bigram_table[prev_tag][tag] * pi[prev_word][prev_tag])
-                if pi[word][tag] < prob:
+                
+                if (pi[word][tag] < prob):
                     pi[word][tag] = prob
                     max_tag = prev_tag
 
-            bp[word][tag] = (prev_word,max_tag)
-                
-            prev_word = word
-    
-    print(bp)
+            bp[word][tag] = max_tag
 
-    
+        prev_word = word
+
+    result = []
+    tag = "</s>"
+    for i in range(len(sentence)-1,-1,-1):
+        result.append((sentence[i],tag))
+        tag = bp[sentence[i]][tag]
+        
+
+    return result[::-1]
