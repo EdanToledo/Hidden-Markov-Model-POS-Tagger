18c729c18f4e5613777e59a0b5d0159c7548a5a0
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-05-29 23:25:09 +0200

    finish trigram implementation

--- main.py
+++ main.py
@@ -2,41 +2,19 @@ import utils
 import wandb
 import argparse
 
-def trigram():
-    training_sentences ,training_word_count = utils.read_csv("Training.csv")
-   
-    training_sentences = utils.convert_to_unk(training_sentences,training_word_count,2)
-
-    total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(training_sentences)
-    total_tag_counts, triple_tag_counts, word_tag_counts = utils.get_second_order_counts(training_sentences)
-
-    testing_sentences = training_sentences
-   
-    tot_count=0
-    tot_ground=0
-    print("Testing on the testing set...\n")
-    for i,(sentence) in enumerate(testing_sentences):
-        print(i)
-        sentence_words = [word for (word,_) in sentence]
-        
-        result = utils.viterbi_trigram(sentence_words, double_tag_counts, triple_tag_counts, word_tag_counts, total_tag_counts)
-        
-        count,total = (utils.eval(result,sentence))
-        tot_count += count
-        tot_ground +=total
-        print("The models shows a",tot_count/tot_ground*100, " percentage accuracy!")
-
-
-def run(training_file,testing_file,unk_threshold,log_to_wandb):
+def run(training_file,testing_file,use_trigram,unk_threshold,log_to_wandb):
     if log_to_wandb:
         wandb.init(project="POS-TAGGER-HMM")
 
     training_sentences ,training_word_count = utils.read_csv(training_file)
     
     training_sentences = utils.convert_to_unk(training_sentences,training_word_count,unk_threshold)
-
+   
     total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(training_sentences)
     
+    if use_trigram:
+        total_tag_counts, triple_tag_counts, word_tag_counts = utils.get_second_order_counts(training_sentences)
+
     
     testing_sentences ,_ = utils.read_csv(testing_file)
 
@@ -48,7 +26,10 @@ def run(training_file,testing_file,unk_threshold,log_to_wandb):
     for i,(sentence) in enumerate(testing_sentences):
         sentence_words = [word for (word,_) in sentence]
         
-        result = utils.viterbi(sentence_words,double_tag_counts,word_tag_counts,total_tag_counts)
+        if use_trigram:
+            result = utils.viterbi_trigram(sentence_words, double_tag_counts, triple_tag_counts, word_tag_counts, total_tag_counts)
+        else:
+            result = utils.viterbi(sentence_words,double_tag_counts,word_tag_counts,total_tag_counts)
         
         count,total = (utils.eval(result,sentence))
         tot_count += count
@@ -65,12 +46,16 @@ if __name__ == "__main__":
     parser.add_argument('--unk_threshold',"-u",default=1,type=int,help='Lower bound of word frequency before being regarded as <UNK>')
     parser.add_argument('--training_file',"-tr",default="Training.csv",type=str,help='Name of training file')
     parser.add_argument('--testing_file',"-te",default="TestSet.csv",type=str,help='Name of testing file')
+    parser.add_argument('--use_trigram',"-ut",default=False,type=bool,help='Use trigram HMM model')
     
     args = parser.parse_args()
     
     unk_threshold = args.unk_threshold
     training_file = args.training_file
     testing_file = args.testing_file
+    use_trigram = args.use_trigram
 
-    run(training_file,testing_file,unk_threshold,True)
+    run(training_file,testing_file,use_trigram,unk_threshold,True)
+
+  
     
--- utils.py
+++ utils.py
@@ -4,46 +4,48 @@ import os
 from collections import defaultdict
 import math
 import csv
+from collections import deque
 
 
-def write_csv(sentences,filename):
+def write_csv(sentences, filename):
     with open(filename+'.csv', mode='w') as csv_file:
         file_writer = csv.writer(csv_file, delimiter=',')
         file_writer.writerow(['Token', 'POS'])
         for sentence in sentences:
-            for (word,tag) in sentence:
+            for (word, tag) in sentence:
                 if word != "<s>" and word != "</s>" and word != "<UNK>":
-                    file_writer.writerow([word,tag])
+                    file_writer.writerow([word, tag])
             file_writer.writerow(['', ''])
+
 
 def read_csv(filename):
     sentences = []
     sen = []
     word_count = defaultdict(int)
-    sen.append(("<s>","START")) 
-    word_count["</s>"]+=1
+    sen.append(("<s>", "START"))
+    word_count["</s>"] += 1
     with open(filename) as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter =",")
+        csv_reader = csv.reader(csv_file, delimiter=",")
 
-        for i,row in enumerate(csv_reader):
+        for i, row in enumerate(csv_reader):
             if i == 0:
                 pass
             else:
                 if row[0] == '':
-                    word_count["</s>"]+=1
-                    sen.append(("</s>","END"))
+                    word_count["</s>"] += 1
+                    sen.append(("</s>", "END"))
                     sentences.append(sen)
 
-                    sen = []   
-                    sen.append(("<s>","START")) 
-                    word_count["<s>"]+=1
+                    sen = []
+                    sen.append(("<s>", "START"))
+                    word_count["<s>"] += 1
                 else:
-                    word_count[row[0].lower()]+=1
+                    word_count[row[0].lower()] += 1
                     sen.append((row[0].lower(), row[1]))
-    word_count["</s>"]+=1
-    sen.append(("</s>","END"))
-    sentences.append(sen) 
-    return sentences , word_count
+    word_count["</s>"] += 1
+    sen.append(("</s>", "END"))
+    sentences.append(sen)
+    return sentences, word_count
 
 
 def split_train_dev(sentences, percentage):
@@ -51,19 +53,19 @@ def split_train_dev(sentences, percentage):
 
     return train, dev
 
-def convert_to_unk(sentences, word_counts,threshold):
+
+def convert_to_unk(sentences, word_counts, threshold):
     new_sentences = []
     for sentence in sentences:
         new_sentence = []
-        for (word,tag) in sentence:
-            if word_counts[word]>threshold:
-                new_sentence.append((word,tag))
+        for (word, tag) in sentence:
+            if word_counts[word] > threshold:
+                new_sentence.append((word, tag))
             else:
-                new_sentence.append(("<UNK>",tag))
+                new_sentence.append(("<UNK>", tag))
         new_sentences.append(new_sentence)
-    
-    return new_sentences 
 
+    return new_sentences
 
 
 def get_vocab_counts(training_sentences):
@@ -72,73 +74,79 @@ def get_vocab_counts(training_sentences):
     word_tag_counts = defaultdict(int)
 
     for sentence in training_sentences:
-        total_tag_counts["START"]+=1
+        total_tag_counts["START"] += 1
         prev_tag = "START"
-        word_tag_counts[("<s>","START")]+=1
-        for i in range(1,len(sentence)):
-            (word,tag) = sentence[i]
-            total_tag_counts[tag]+=1
-            double_tag_counts[(prev_tag,tag)]+=1
-            word_tag_counts[(word,tag)]+=1
+        word_tag_counts[("<s>", "START")] += 1
+        for i in range(1, len(sentence)):
+            (word, tag) = sentence[i]
+            total_tag_counts[tag] += 1
+            double_tag_counts[(prev_tag, tag)] += 1
+            word_tag_counts[(word, tag)] += 1
             prev_tag = tag
 
     return total_tag_counts, double_tag_counts, word_tag_counts
 
 
-def get_bigram_prob_laplace(prev_tag,tag,double_tag_counts,total_tag_counts):
-    if ((prev_tag,tag) in double_tag_counts):
-        return (double_tag_counts[(prev_tag,tag)]+1)/(total_tag_counts[prev_tag] + len(total_tag_counts))
+def get_bigram_prob_laplace(prev_tag, tag, double_tag_counts, total_tag_counts):
+    if ((prev_tag, tag) in double_tag_counts):
+        return (double_tag_counts[(prev_tag, tag)]+1)/(total_tag_counts[prev_tag] + len(total_tag_counts))
     else:
         return 1/(total_tag_counts[prev_tag] + len(total_tag_counts))
 
 
-def get_emission_prob(word,tag,word_tag_counts,total_tag_counts):
-    if ((word,tag) in word_tag_counts):
-        return word_tag_counts[(word,tag)]/total_tag_counts[tag]
+def get_emission_prob(word, tag, word_tag_counts, total_tag_counts):
+    if ((word, tag) in word_tag_counts):
+        return word_tag_counts[(word, tag)]/total_tag_counts[tag]
     else:
         return 0
 
-def get_emission_prob_table(word_tag_counts,total_tag_counts):
-    table = defaultdict(lambda : defaultdict(float))
-    for (word,_) in word_tag_counts:
-        
-        for tag in total_tag_counts:
-            table[word][tag] = get_emission_prob(word,tag,word_tag_counts,total_tag_counts)
 
-        
+def get_emission_prob_table(word_tag_counts, total_tag_counts):
+    table = defaultdict(lambda: defaultdict(float))
+    for (word, _) in word_tag_counts:
+
+        for tag in total_tag_counts:
+            table[word][tag] = get_emission_prob(
+                word, tag, word_tag_counts, total_tag_counts)
+
     return table
 
-def get_bigram_prob_table(double_tag_counts,total_tag_counts,bigram_prob_func):
-    table = defaultdict(lambda : defaultdict(float))
+
+def get_bigram_prob_table(double_tag_counts, total_tag_counts, bigram_prob_func):
+    table = defaultdict(lambda: defaultdict(float))
     for prev_tag in total_tag_counts:
         for tag in total_tag_counts:
-            table[prev_tag][tag] = bigram_prob_func(prev_tag,tag,double_tag_counts,total_tag_counts)
-       
+            table[prev_tag][tag] = bigram_prob_func(
+                prev_tag, tag, double_tag_counts, total_tag_counts)
+
     return table
 
-def linear_interpolation_smoothing(bi_lamba,bi_prob,uni_lambda,uni_prob):
+
+def linear_interpolation_smoothing(bi_lamba, bi_prob, uni_lambda, uni_prob):
     return bi_lamba*bi_prob+uni_lambda*uni_prob
 
 
 def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
     pi = [defaultdict(float)]
     bp = [{}]
-    emission_table = get_emission_prob_table(word_tag_counts,total_tag_counts)
-    bigram_table = get_bigram_prob_table(double_tag_counts,total_tag_counts,get_bigram_prob_laplace)
+    emission_table = get_emission_prob_table(word_tag_counts, total_tag_counts)
+    bigram_table = get_bigram_prob_table(
+        double_tag_counts, total_tag_counts, get_bigram_prob_laplace)
 
     prev_tag = "START"
     pi[0][prev_tag] = 1
 
-    for i in range(1,len(sentence)):
+    for i in range(1, len(sentence)):
         word = sentence[i]
         pi.append(defaultdict(float))
         bp.append({})
         for tag in total_tag_counts:
-            
+
             for prev_tag in total_tag_counts:
-               
-                prob = (emission_table[word][tag] * bigram_table[prev_tag][tag] * pi[i-1][prev_tag])
-               
+
+                prob = (
+                    emission_table[word][tag] * bigram_table[prev_tag][tag] * pi[i-1][prev_tag])
+
                 # if pi[i-1][prev_tag]>0:
                 #     print("WORD NUMBER:",i,"out of",len(sentence))
                 #     print(word,tag,"emmission:",emission_table[word][tag])
@@ -149,26 +157,27 @@ def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
                     pi[i][tag] = prob
                     bp[i][tag] = prev_tag
 
-            
     result = []
     tag = "END"
-    for i in range(len(sentence)-1,0,-1):
-        result.append((sentence[i],tag))
+    for i in range(len(sentence)-1, 0, -1):
+        result.append((sentence[i], tag))
         tag = bp[i][tag]
-    result.append(("<s>","START"))
+    result.append(("<s>", "START"))
 
     return result[::-1]
 
-def eval(result,ground_truth):
+
+def eval(result, ground_truth):
     count_true = 0
-    for i,(word,pos) in enumerate(result):
-        (true_word,true_pos) = ground_truth[i]
+    for i, (word, pos) in enumerate(result):
+        (true_word, true_pos) = ground_truth[i]
         # if pos!=true_pos:
         #     print(pos,true_pos)
         if word == true_word and pos == true_pos:
-            count_true+=1
-    
-    return count_true,len(ground_truth)
+            count_true += 1
+
+    return count_true, len(ground_truth)
+
 
 def get_second_order_counts(training_sentences):
     total_tag_counts = defaultdict(int)
@@ -176,77 +185,102 @@ def get_second_order_counts(training_sentences):
     word_tag_counts = defaultdict(int)
 
     for i in range(len(training_sentences)):
-        total_tag_counts["START"]+=1
+        total_tag_counts["START"] += 1
         prev_prev_tag = "START"
-        word_tag_counts[("<s>","START")]+=1
+        word_tag_counts[("<s>", "START")] += 1
 
-        (word,prev_tag) = training_sentences[i][1]
+        (word, prev_tag) = training_sentences[i][1]
         total_tag_counts[prev_tag] += 1
-        word_tag_counts[(word,prev_tag)]+= 1
+        word_tag_counts[(word, prev_tag)] += 1
 
-        for j in range(2,len(training_sentences[i])):
-            (word,tag) = training_sentences[i][j]
-            total_tag_counts[tag]+=1
-            triple_tag_counts[(prev_prev_tag,prev_tag,tag)]+=1
-            word_tag_counts[(word,tag)]+=1
+        for j in range(2, len(training_sentences[i])):
+            (word, tag) = training_sentences[i][j]
+            total_tag_counts[tag] += 1
+            triple_tag_counts[(prev_prev_tag, prev_tag, tag)] += 1
+            word_tag_counts[(word, tag)] += 1
             prev_prev_tag = prev_tag
             prev_tag = tag
 
     return total_tag_counts, triple_tag_counts, word_tag_counts
 
-def get_trigram_laplace_prob(prev_prev_tag,prev_tag,tag,triple_tag_counts,bigram_count,total_tag_counts):
 
-    if ((prev_prev_tag,prev_tag,tag) in triple_tag_counts):
+def get_trigram_laplace_prob(prev_prev_tag, prev_tag, tag, triple_tag_counts, bigram_count, total_tag_counts):
 
-        return (triple_tag_counts[(prev_prev_tag, prev_tag,tag)]+1)/(bigram_count[prev_prev_tag][prev_tag] + len(total_tag_counts))
+    if ((prev_prev_tag, prev_tag, tag) in triple_tag_counts):
+
+        return (triple_tag_counts[(prev_prev_tag, prev_tag, tag)]+1)/(bigram_count[prev_prev_tag][prev_tag] + len(total_tag_counts))
 
     else:
 
         return 1/(total_tag_counts[prev_tag] + len(total_tag_counts))
 
 
-def get_trigram_prob_table(triple_tag_counts,bigram_counts,total_tag_counts,trigram_prob_func):
-    table = defaultdict(lambda : defaultdict(lambda : defaultdict(float)))
+def get_trigram_prob_table(triple_tag_counts, bigram_counts, total_tag_counts, trigram_prob_func):
+    table = defaultdict(lambda: defaultdict(lambda: defaultdict(float)))
     for prev_prev_tag in total_tag_counts:
         for prev_tag in total_tag_counts:
             for tag in total_tag_counts:
-                table[prev_prev_tag][prev_tag][tag] = trigram_prob_func(prev_tag,prev_tag,tag,triple_tag_counts,bigram_counts,total_tag_counts)
-       
+                table[prev_prev_tag][prev_tag][tag] = trigram_prob_func(
+                    prev_tag, prev_tag, tag, triple_tag_counts, bigram_counts, total_tag_counts)
+
     return table
 
+
 def viterbi_trigram(sentence, double_tag_counts, triple_tag_counts, word_tag_counts, total_tag_counts):
-    pi = defaultdict(float)
-    bp = defaultdict(float)
-    emission_table = get_emission_prob_table(word_tag_counts,total_tag_counts)
-    bigram_table = get_bigram_prob_table(double_tag_counts,total_tag_counts,get_bigram_prob_laplace)
-    trigram_table = get_trigram_prob_table(triple_tag_counts, bigram_table, total_tag_counts, get_trigram_laplace_prob)
+    pi = [defaultdict(float)]
+    bp = [{}]
+    emission_table = get_emission_prob_table(word_tag_counts, total_tag_counts)
+    bigram_table = get_bigram_prob_table(
+        double_tag_counts, total_tag_counts, get_bigram_prob_laplace)
+    trigram_table = get_trigram_prob_table(
+        triple_tag_counts, bigram_table, total_tag_counts, get_trigram_laplace_prob)
 
-    pi[(0,"START", "START")] = 1
-    
-    for i in range(1,len(sentence)):
+    pi[0][("START", "START")] = 1
+
+    # OPTIMIZES NUMBER OF LOOPS NECESSARY
+    def tag_subsets(k):
+        if k in (-1, 0):
+            return {"START"}
+        else:
+            return total_tag_counts
+
+    for i in range(1, len(sentence)):
         word = sentence[i]
+        pi.append(defaultdict(float))
+        bp.append({})
+        for prev_tag in tag_subsets(i-1):
+            for tag in total_tag_counts:
+                for prev_prev_tag in tag_subsets(i-2):
+                    if emission_table[word][tag] != 0:
+                        prob = pi[i-1][(prev_prev_tag, prev_tag)] * \
+                            trigram_table[prev_prev_tag][prev_tag][tag] * \
+                            emission_table[word][tag]
 
+                        if prob > pi[i][(prev_tag, tag)]:
+                            pi[i][(prev_tag, tag)] = prob
+                            bp[i][(prev_tag, tag)] = prev_prev_tag
+
+    max_score = float('-Inf')
+    prev_max_tag, tag_max_tag = None, None
+    for prev_tag in total_tag_counts:
         for tag in total_tag_counts:
-            for prev_tag in total_tag_counts:
-                max_score = -1
-                max_tag = None
-                for prev_prev_tag in total_tag_counts:
-                    print("here")
-                    prob = emission_table[word][tag] * trigram_table[prev_prev_tag][prev_tag][tag] * pi[(i-1,prev_prev_tag,prev_tag)]
-                    print("here")
+            prob = pi[len(sentence)-1][(prev_tag, tag)] * \
+                trigram_table[prev_tag][tag]["END"]
 
-                    if (max_score < prob):
-                        max_score = prob
-                        max_tag = prev_prev_tag
-                        
-                pi[(i,prev_tag,tag)] = max_score
-                bp[(i,prev_tag,tag)] = max_tag
-            
+            if prob > max_score:
+                max_score = prob
+                prev_max_tag = prev_tag
+                tag_max_tag = tag
+
+    tag = tag_max_tag
+    prev_tag = prev_max_tag
+
     result = []
-    tag = "END"
-    for i in range(len(sentence)-1,0,-1):
-        result.append((sentence[i],tag))
-        tag = bp[i][tag]
-    result.append(("<s>","START"))
+    for i in range(len(sentence)-1, 0, -1):
+        result.append((sentence[i], tag))
+        temp = prev_tag
+        prev_tag = bp[i][(prev_tag, tag)]
+        tag = temp
+    result.append(("<s>", "START"))
 
     return result[::-1]
