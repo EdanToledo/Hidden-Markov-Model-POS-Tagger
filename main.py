79f449430bc8c034fa8842fdbc07f705d547ec76
Edan Toledo <TLDEDA001@myuct.ac.za>
2021-05-29 18:18:34 +0200

    fix viterbi bug

--- main.py
+++ main.py
@@ -2,14 +2,22 @@ import utils
 import pandas as pd
 
 def main():
-    sentences = utils.read_csv("Training.csv")
-   
-    total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(sentences)
+
+    training_sentences ,training_word_count = utils.read_csv("Training.csv")
+    training_sentences = utils.convert_to_unk(training_sentences,training_word_count,1)
+
+    total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(training_sentences)
     
+    testing_sentences = training_sentences
+    
+    testing_sentences ,_ = utils.read_csv("TestSet.csv")
+
+    testing_sentences = utils.convert_to_unk(testing_sentences,training_word_count,0)
+   
     tot_count=0
     tot_ground=0
     print("Testing on the testing set...\n")
-    for i,(sentence) in enumerate(sentences):
+    for i,(sentence) in enumerate(testing_sentences):
         sentence_words = [word for (word,_) in sentence]
         result = utils.viterbi(sentence_words,double_tag_counts,word_tag_counts,total_tag_counts)
         
--- utils.py
+++ utils.py
@@ -9,7 +9,9 @@ import csv
 def read_csv(filename):
     sentences = []
     sen = []
+    word_count = defaultdict(int)
     sen.append(("<s>","START")) 
+    word_count["</s>"]+=1
     with open(filename) as csv_file:
         csv_reader = csv.reader(csv_file, delimiter =",")
 
@@ -18,22 +20,40 @@ def read_csv(filename):
                 pass
             else:
                 if row[0] == '':
+                    word_count["</s>"]+=1
                     sen.append(("</s>","END"))
                     sentences.append(sen)
 
                     sen = []   
                     sen.append(("<s>","START")) 
+                    word_count["<s>"]+=1
                 else:
+                    word_count[row[0].lower()]+=1
                     sen.append((row[0].lower(), row[1]))
+    word_count["</s>"]+=1
     sen.append(("</s>","END"))
     sentences.append(sen) 
-    return sentences
+    return sentences , word_count
 
 
 def split_train_dev(sentences, percentage):
     train, dev = train_test_split(sentences, test_size=percentage)
 
     return train, dev
+
+def convert_to_unk(sentences, word_counts,threshold):
+    new_sentences = []
+    for sentence in sentences:
+        new_sentence = []
+        for (word,tag) in sentence:
+            if word_counts[word]>threshold:
+                new_sentence.append((word,tag))
+            else:
+                new_sentence.append(("<UNK>",tag))
+        new_sentences.append(new_sentence)
+    
+    return new_sentences 
+
 
 
 def get_vocab_counts(training_sentences):
@@ -45,10 +65,9 @@ def get_vocab_counts(training_sentences):
         total_tag_counts["START"]+=1
         prev_tag = "START"
         word_tag_counts[("<s>","START")]+=1
-        for i,(word,tag) in enumerate(sentence):
-            if i == 0:
-                continue
-            
+        for i in range(1,len(sentence)):
+            (word,tag) = sentence[i]
+
             total_tag_counts[tag]+=1
             double_tag_counts[(prev_tag,tag)]+=1
             word_tag_counts[(word,tag)]+=1
@@ -92,7 +111,7 @@ def get_bigram_prob_table(double_tag_counts,total_tag_counts,bigram_prob_func):
 def linear_interpolation_smoothing(bi_lamba,bi_prob,uni_lambda,uni_prob):
     return bi_lamba*bi_prob+uni_lambda*uni_prob
 
-# what about y0 == <s>
+
 def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
     pi = [defaultdict(float)]
     bp = [{}]
@@ -101,7 +120,7 @@ def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
 
     prev_tag = "START"
     pi[0][prev_tag] = 1
-    
+
     for i in range(1,len(sentence)):
         word = sentence[i]
         pi.append(defaultdict(float))
@@ -111,14 +130,12 @@ def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
             for prev_tag in total_tag_counts:
                
                 prob = (emission_table[word][tag] * bigram_table[prev_tag][tag] * pi[i-1][prev_tag])
-                
-                if (pi[i][tag] < prob):
+
+                if (pi[i][tag] <= prob):
                     pi[i][tag] = prob
                     bp[i][tag] = prev_tag
 
             
-
-
     result = []
     tag = "END"
     for i in range(len(sentence)-1,0,-1):
