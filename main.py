8553ed43c5846f95d56fd7dc8076c84fa976db89
RoyCohenGit <rtcohen99@gmail.com>
2021-05-29 21:00:49 +0200

    Start second order HMM tagging

--- main.py
+++ main.py
@@ -1,6 +1,30 @@
 import utils
 import pandas as pd
 
+def trigram():
+    training_sentences ,training_word_count = utils.read_csv("Training.csv")
+   
+    training_sentences = utils.convert_to_unk(training_sentences,training_word_count,2)
+
+    total_tag_counts, double_tag_counts, word_tag_counts = utils.get_vocab_counts(training_sentences)
+    total_tag_counts, triple_tag_counts, word_tag_counts = utils.get_second_order_counts(training_sentences)
+
+    testing_sentences = training_sentences
+   
+    tot_count=0
+    tot_ground=0
+    print("Testing on the testing set...\n")
+    for i,(sentence) in enumerate(testing_sentences):
+        print(i)
+        sentence_words = [word for (word,_) in sentence]
+        
+        result = utils.viterbi_trigram(sentence_words, double_tag_counts, triple_tag_counts, word_tag_counts, total_tag_counts)
+        
+        count,total = (utils.eval(result,sentence))
+        tot_count += count
+        tot_ground +=total
+        print("The models shows a",tot_count/tot_ground*100, " percentage accuracy!")
+
 def main():
 
     training_sentences ,training_word_count = utils.read_csv("Training.csv")
@@ -35,4 +59,4 @@ def main():
         
     
 if __name__ == "__main__":
-    main()
+    trigram()
--- utils.py
+++ utils.py
@@ -60,14 +60,13 @@ def get_vocab_counts(training_sentences):
     total_tag_counts = defaultdict(int)
     double_tag_counts = defaultdict(int)
     word_tag_counts = defaultdict(int)
-    
+
     for sentence in training_sentences:
         total_tag_counts["START"]+=1
         prev_tag = "START"
         word_tag_counts[("<s>","START")]+=1
         for i in range(1,len(sentence)):
             (word,tag) = sentence[i]
-
             total_tag_counts[tag]+=1
             double_tag_counts[(prev_tag,tag)]+=1
             word_tag_counts[(word,tag)]+=1
@@ -101,8 +100,7 @@ def get_emission_prob_table(word_tag_counts,total_tag_counts):
 
 def get_bigram_prob_table(double_tag_counts,total_tag_counts,bigram_prob_func):
     table = defaultdict(lambda : defaultdict(float))
-    for (prev_tag,_) in double_tag_counts:
-        
+    for prev_tag in total_tag_counts:
         for tag in total_tag_counts:
             table[prev_tag][tag] = bigram_prob_func(prev_tag,tag,double_tag_counts,total_tag_counts)
        
@@ -151,8 +149,6 @@ def viterbi(sentence, double_tag_counts, word_tag_counts, total_tag_counts):
 
     return result[::-1]
 
-
-
 def eval(result,ground_truth):
     count_true = 0
     for i,(word,pos) in enumerate(result):
@@ -163,3 +159,86 @@ def eval(result,ground_truth):
             count_true+=1
     
     return count_true,len(ground_truth)
+
+def get_second_order_counts(training_sentences):
+    total_tag_counts = defaultdict(int)
+    triple_tag_counts = defaultdict(int)
+    word_tag_counts = defaultdict(int)
+
+    for i in range(len(training_sentences)):
+        total_tag_counts["START"]+=1
+        prev_prev_tag = "START"
+        word_tag_counts[("<s>","START")]+=1
+
+        (word,prev_tag) = training_sentences[i][1]
+        total_tag_counts[prev_tag] += 1
+        word_tag_counts[(word,prev_tag)]+= 1
+
+        for j in range(2,len(training_sentences[i])):
+            (word,tag) = training_sentences[i][j]
+            total_tag_counts[tag]+=1
+            triple_tag_counts[(prev_prev_tag,prev_tag,tag)]+=1
+            word_tag_counts[(word,tag)]+=1
+            prev_prev_tag = prev_tag
+            prev_tag = tag
+
+    return total_tag_counts, triple_tag_counts, word_tag_counts
+
+def get_trigram_laplace_prob(prev_prev_tag,prev_tag,tag,triple_tag_counts,bigram_count,total_tag_counts):
+
+    if ((prev_prev_tag,prev_tag,tag) in triple_tag_counts):
+
+        return (triple_tag_counts[(prev_prev_tag, prev_tag,tag)]+1)/(bigram_count[prev_prev_tag][prev_tag] + len(total_tag_counts))
+
+    else:
+
+        return 1/(total_tag_counts[prev_tag] + len(total_tag_counts))
+
+
+def get_trigram_prob_table(triple_tag_counts,bigram_counts,total_tag_counts,trigram_prob_func):
+    table = defaultdict(lambda : defaultdict(lambda : defaultdict(float)))
+    for prev_prev_tag in total_tag_counts:
+        for prev_tag in total_tag_counts:
+            for tag in total_tag_counts:
+                table[prev_prev_tag][prev_tag][tag] = trigram_prob_func(prev_tag,prev_tag,tag,triple_tag_counts,bigram_counts,total_tag_counts)
+       
+    return table
+
+def viterbi_trigram(sentence, double_tag_counts, triple_tag_counts, word_tag_counts, total_tag_counts):
+    pi = defaultdict(float)
+    bp = [{}]
+    emission_table = get_emission_prob_table(word_tag_counts,total_tag_counts)
+    bigram_table = get_bigram_prob_table(double_tag_counts,total_tag_counts,get_bigram_prob_laplace)
+    trigram_table = get_trigram_prob_table(triple_tag_counts, bigram_table, total_tag_counts, get_trigram_laplace_prob)
+
+    pi[(0,"START", "START")] = 1
+    
+    for i in range(1,len(sentence)):
+        word = sentence[i]
+        bp.append({})
+
+        for tag in total_tag_counts:
+            for prev_tag in total_tag_counts:
+               for prev_prev_tag in total_tag_counts:
+                    print("here")
+                    prob = emission_table[word][tag] * trigram_table+[prev_prev_tag][prev_tag][tag] * pi.get(i-1,prev_prev_tag,prev_tag)
+               
+                    # if pi[i-1][prev_tag]>0:
+                    #     print("WORD NUMBER:",i,"out of",len(sentence))
+                    #     print(word,tag,"emmission:",emission_table[word][tag])
+                    #     print(prev_tag,tag,"bigram:",bigram_table[prev_tag][tag])
+                    #     print(sentence[i-1],prev_tag,"pi:",pi[i-1][prev_tag])
+
+                    if (pi[i][tag] < prob):
+                        pi[i][tag] = prob
+                        bp[i][tag] = prev_tag
+
+            
+    result = []
+    tag = "END"
+    for i in range(len(sentence)-1,0,-1):
+        result.append((sentence[i],tag))
+        tag = bp[i][tag]
+    result.append(("<s>","START"))
+
+    return result[::-1]
